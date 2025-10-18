ae37491b304af6b0abf2e07acb882b8fb24e6040
Tom Larkworthy <tom.larkworthy@gmail.com>
2025-10-18 14:41:25 +0200

    Update

--- src/build.sh
+++ src/build.sh
@@ -4,6 +4,11 @@ esbuild node_modules/@observablehq/runtime/src/index.js \
     --bundle \
     --format=esm | gzip | base64 > assets/@observablehq_runtime@6.0.0.js.gz.b64
 
+esbuild node_modules/@observablehq/inspector/src/index.js \
+    --platform=browser \
+    --bundle \
+    --format=esm | gzip | base64 > assets/@observablehq_inspector@5.0.1.js.gz.b64
+
 esbuild node_modules/es-module-shims/dist/es-module-shims.js \
     --platform=browser \
     --bundle \

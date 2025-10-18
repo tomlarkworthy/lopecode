75a00ebeb751d3a8a150d4cbc38bad145ae65152
Tom Larkworthy <tom.larkworthy@gmail.com>
2025-10-18 14:11:11 +0200

    Unminify Observable Runtime

--- src/build.sh
+++ src/build.sh
@@ -2,7 +2,12 @@ rm -fR assets/*
 esbuild node_modules/@observablehq/runtime/src/index.js \
     --platform=browser \
     --bundle \
+    --format=esm | gzip | base64 > assets/@observablehq_runtime@6.0.0.js.gz.b64
+
+esbuild node_modules/es-module-shims/dist/es-module-shims.js \
+    --platform=browser \
+    --bundle \
     --format=esm \
-    --minify | gzip | base64 > assets/@observablehq_runtime@6.0.0.js.gz.b64
+    --minify | gzip | base64 > assets/es-module-shims@2.6.2.js.gz.b64
 
 ls -ll assets/
